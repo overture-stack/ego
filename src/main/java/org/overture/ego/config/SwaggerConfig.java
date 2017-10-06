52fa13f9065d54aaf404ded1385bc28cefc500e5
Rahul Verma <Rahul.Verma@oicr.on.ca>
2017-10-06 13:28:43 -0400

    Fixed a typo

--- src/main/java/org/overture/ego/config/SwaggerConfig.java
+++ src/main/java/org/overture/ego/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.kidsfirst.authorizationservice.controller"))
+                .apis(RequestHandlerSelectors.basePackage("org.overture.ego.controller"))
                 .build()
                 .apiInfo(metaInfo());
     }
