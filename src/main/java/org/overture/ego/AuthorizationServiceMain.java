7250601bc14fe8b474323e269b121d88c7f6e811
Rahul Verma <Rahul.Verma@oicr.on.ca>
2017-11-22 17:17:27 -0500

    Cleanup - 2

--- src/main/java/org/overture/ego/AuthorizationServiceMain.java
+++ src/main/java/org/overture/ego/AuthorizationServiceMain.java
@@ -18,11 +18,6 @@ package org.overture.ego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 public class AuthorizationServiceMain {
