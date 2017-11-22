f8b947e6baf133ba293a1ed989dd8a1a42b4d5fc
Jon Eubank <joneubank@gmail.com>
2017-11-22 14:23:40 -0500

    [Ego-31] CircleCI deploy.sh Progress output statements

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -16,18 +16,28 @@
 #  - EGO_ENV_FILE_PATH                                    #
 #                                                         #
 # Also before running, on the deploy server:              #
-#   - configure all env variables listed in               #
-#      /env_template.sh so that the start & stop scripts  #
-#      will run.                                          #
+#   - create a file on the server that configures all     #
+#      environment variables listed in /env_template.sh ; #
+#      These values are needed for that the start & stop  #
+#      scripts will run ; add the path to this file on    #
+#      the server in the CircleCI environment variable:   #
+#        EGO_ENV_FILE_PATH                                #
 #   - create an install path owned by the EGO_DEPLOY_USER #
 #   - create a `builds` directory inside the install path #
 #                                                         #
 ### =================================================== ###
 
-# === Sync Resource Files
-rsync -azP src/main/resources "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER:$EGO_INSTALL_PATH"
 
-# === Find generated filename
+# === Sync Resource Files
+echo "# ===== START rsync of resource files ===== #"
+rsync -azP src/main/resources "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER:$EGO_INSTALL_PATH"
+echo "# ===== END rsync of resource files ===== #\n\n"
+
+
+# === Upload Build
+echo "# ===== START Upload build package ===== #"
+
+# = Find generated filename
 for i in target/ego-*.tar.gz; do
     filepath=$i;
 done
@@ -36,18 +46,32 @@ echo "filepath: $filepath"
 file=$(basename $filepath)
 echo "file: $file"
 
-# === Upload New Uber-Jar
+# = scp file to server
+echo "Upload Starting:"
 scp $filepath "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER:$EGO_INSTALL_PATH/builds/"
 
+echo "# ===== END Upload build package ===== #\n\n"
+
+
 # === Unpack Jar
+echo "# ===== START Unpack jar ===== #"
 ssh "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER" "tar zxvf $EGO_INSTALL_PATH/builds/$file -C $EGO_INSTALL_PATH/builds"
+echo "# ===== END Unpack jar ===== #\n\n"
 
 # === Stop Existing Service
+echo "# ===== START Stop server ===== #"
 ssh "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER" "source $EGO_ENV_FILE_PATH; $EGO_INSTALL_PATH/resources/scripts/stop-server.sh"
+echo "# ===== END Stop server ===== #\n\n"
 
 # === Update Symlink
+echo "# ===== START Update Installed version link ===== #"
 extract_folder="${file%-dist.tar.gz}"
 ssh "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER" "ln -sf $EGO_INSTALL_PATH/builds/$extract_folder $EGO_INSTALL_PATH/install"
+echo "# ===== END Update Installed version link ===== #\n\n"
 
 # === Start Existing Service
+echo "# ===== START Start Server ===== #"
 ssh "$EGO_DEPLOY_USER@$EGO_DEPLOY_SERVER" "source $EGO_ENV_FILE_PATH; $EGO_INSTALL_PATH/resources/scripts/start-server.sh"
+echo "# ===== END Start Server ===== #"
+
+echo "\n\n\n# ============== SUCCESS!"
