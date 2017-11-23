124891321e4512ec60c72b1c8f48af692c89097c
Jon Eubank <joneubank@gmail.com>
2017-11-23 10:03:20 -0500

    [Ego-30] Update Dummy Data scripts to refactored schema

--- src/main/resources/dummy-data/03-insert-dummy-applications.sql
+++ src/main/resources/dummy-data/03-insert-dummy-applications.sql
@@ -4,7 +4,7 @@ Clears the EgoApplication table and insert 4 sample Applications (3 Approved, 1 
 */
 TRUNCATE public.egoapplication CASCADE;
 
-INSERT INTO EGOAPPLICATION (appName, clientId, clientSecret, redirectUri, description, status) VALUES ('Example Data Portal', 'sample-data-portal', 'sample-data-portal-secret', 'http://google.com', 'Sample application for some data portal', 'Approved');
-INSERT INTO EGOAPPLICATION (appName, clientId, clientSecret, redirectUri, description, status) VALUES ('Personal Information Manager', 'personal-info-manager', 'personal-info-manager-secret', 'http://yahoo.com', 'Sample application for some user manager', 'Approved');
-INSERT INTO EGOAPPLICATION (appName, clientId, clientSecret, redirectUri, description, status) VALUES ('Daily News Feed', 'daily-news-feed', 'daily-news-feed-secret', 'http://bing.com', 'Sample application for some news feed', 'Approved');
-INSERT INTO EGOAPPLICATION (appName, clientId, clientSecret, redirectUri, description, status) VALUES ('User Notification System', 'user-notification-system', 'user-notification-system-secret', 'http://aol.com', 'Sample application for a user notification management system', 'Pending');
+INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('Example Data Portal', 'sample-data-portal', 'sample-data-portal-secret', 'http://google.com', 'Sample application for some data portal', 'Approved');
+INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('Personal Information Manager', 'personal-info-manager', 'personal-info-manager-secret', 'http://yahoo.com', 'Sample application for some user manager', 'Approved');
+INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('Daily News Feed', 'daily-news-feed', 'daily-news-feed-secret', 'http://bing.com', 'Sample application for some news feed', 'Approved');
+INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('User Notification System', 'user-notification-system', 'user-notification-system-secret', 'http://aol.com', 'Sample application for a user notification management system', 'Pending');
--- src/main/resources/dummy-data/06-insert-dummy-rel-group-application.sql
+++ src/main/resources/dummy-data/06-insert-dummy-rel-group-application.sql
@@ -1,17 +1,17 @@
 TRUNCATE public.groupapplication CASCADE;
 
 -- Add all Approved groups to Perssonal Information Manager
-INSERT INTO public.groupapplication (grpname, appname)
-  SELECT g.grpname, a.appname
+INSERT INTO public.groupapplication (grpId, appId)
+  SELECT g.id, a.id
   FROM public.egogroup AS g
     LEFT JOIN public.egoapplication AS a
-    ON a.appname='Personal Information Manager'
-  WHERE g.grpname IN ('Pediatric Patient Support Network', 'Extreme Research Consortium', 'XYZ Cancer Research Institute', 'Healthcare Providers Anonymous');
+    ON a.name='Personal Information Manager'
+  WHERE g.name IN ('Pediatric Patient Support Network', 'Extreme Research Consortium', 'XYZ Cancer Research Institute', 'Healthcare Providers Anonymous');
 
 -- Add Research Groups to the Data Portal
-INSERT INTO public.groupapplication (grpname, appname)
-  SELECT g.grpname, a.appname
+INSERT INTO public.groupapplication (grpId, appId)
+  SELECT g.id, a.id
   FROM public.egogroup AS g
     LEFT JOIN public.egoapplication AS a
-      ON a.appname='Example Data Portal'
-  WHERE g.grpname IN ('XYZ Cancer Research Institute', 'Extreme Research Consortium');
+      ON a.name='Example Data Portal'
+  WHERE g.name IN ('XYZ Cancer Research Institute', 'Extreme Research Consortium');
