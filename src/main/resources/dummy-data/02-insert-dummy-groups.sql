5b82737fc740d7f11c25df87ae3219e9a320bd93
Jon Eubank <joneubank@gmail.com>
2017-11-23 10:03:20 -0500

    [Ego-30] Update Dummy Data scripts to refactored schema

--- src/main/resources/dummy-data/02-insert-dummy-groups.sql
+++ src/main/resources/dummy-data/02-insert-dummy-groups.sql
@@ -4,8 +4,8 @@ Clears the EgoGroup table and insert 5 sample groups (4 Approved, 1 Pending)
 */
 TRUNCATE public.egogroup CASCADE;
 
-INSERT INTO egogroup (grpName, status, description) VALUES ('XYZ Cancer Research Institute', 'Approved', 'Sample group for elite cancer researchers');
-INSERT INTO egogroup (grpName, status, description) VALUES ('Extreme Research Consortium', 'Approved', 'Sample group for generalist researchers');
-INSERT INTO egogroup (grpName, status, description) VALUES ('Healthcare Providers Anonymous', 'Approved', 'Sample group for patient care specialist');
-INSERT INTO egogroup (grpName, status, description) VALUES ('Pediatric Patient Support Network', 'Approved', 'Sample group for patients and their supporters');
-INSERT INTO egogroup (grpName, status, description) VALUES ('Generic Genomics Geniuses', 'Pending', 'Sample group for super-duper smart genetic investigators');
+INSERT INTO egogroup (name, status, description) VALUES ('XYZ Cancer Research Institute', 'Approved', 'Sample group for elite cancer researchers');
+INSERT INTO egogroup (name, status, description) VALUES ('Extreme Research Consortium', 'Approved', 'Sample group for generalist researchers');
+INSERT INTO egogroup (name, status, description) VALUES ('Healthcare Providers Anonymous', 'Approved', 'Sample group for patient care specialist');
+INSERT INTO egogroup (name, status, description) VALUES ('Pediatric Patient Support Network', 'Approved', 'Sample group for patients and their supporters');
+INSERT INTO egogroup (name, status, description) VALUES ('Generic Genomics Geniuses', 'Pending', 'Sample group for super-duper smart genetic investigators');
--- src/main/resources/dummy-data/05-insert-dummy-rel-user-application.sql
+++ src/main/resources/dummy-data/05-insert-dummy-rel-user-application.sql
@@ -1,17 +1,17 @@
 TRUNCATE public.userapplication CASCADE;
 
 -- 6 Users to Personal Information Manager
-INSERT INTO public.userapplication (username, appname)
-  SELECT u.username, a.appname
+INSERT INTO public.userapplication (userId, appId)
+  SELECT u.id, a.id
   FROM public.egouser AS u
     LEFT JOIN public.egoapplication AS a
-      ON a.appname='Personal Information Manager'
-  WHERE u.username IN ('Brennan.Denesik@example.com','Anika.Stehr@example.com','Janessa.Cronin@example.com','Sharon.Farrell@example.com','Zane.Rath@example.com','Elisha.Weimann@example.com');
+      ON a.name='Personal Information Manager'
+  WHERE u.name IN ('Brennan.Denesik@example.com','Anika.Stehr@example.com','Janessa.Cronin@example.com','Sharon.Farrell@example.com','Zane.Rath@example.com','Elisha.Weimann@example.com');
 
 -- 35 Users to Example Data Portal
-INSERT INTO public.userapplication (username, appname)
-  SELECT u.username, a.appname
+INSERT INTO public.userapplication (userId, appId)
+  SELECT u.id, a.id
   FROM public.egouser AS u
     LEFT JOIN public.egoapplication AS a
-      ON a.appname='Example Data Portal'
-  WHERE u.username IN ('Justice.Heller@example.com','Sharon.Farrell@example.com','Janessa.Cronin@example.com','Shayne.Lubowitz@example.com','Gretchen.Wintheiser@example.com','Daija.Pacocha@example.com','Osvaldo.Bahringer@example.com','Halie.Heller@example.com','Chauncey.Schiller@example.com','Oral.Gleason@example.com','Lupe.Hilll@example.com','Jocelyn.Grant@example.com','Hollie.Kunde@example.com','Ed.Olson@example.com','Jeromy.Larkin@example.com','Marquis.Oberbrunner@example.com','Lyda.Macejkovic@example.com','Gordon.Ullrich@example.com','Kenton.Kilback@example.com','Maya.DuBuque@example.com','Jeromy.Abernathy@example.com','Furman.Volkman@example.com','Yesenia.Schmeler@example.com','Waylon.Wiza@example.com','Helen.Trantow@example.com','Claudine.McKenzie@example.com','Korbin.Sawayn@example.com','Brionna.Mertz@example.com','Orin.Mraz@example.com','Rusty.Hickle@example.com','Rafaela.Harvey@example.com','Herminio.Kub@example.com','Lera.White@example.com','Chandler.Collier@example.com','Edd.Thompson@example.com');
+      ON a.name='Example Data Portal'
+  WHERE u.name IN ('Justice.Heller@example.com','Sharon.Farrell@example.com','Janessa.Cronin@example.com','Shayne.Lubowitz@example.com','Gretchen.Wintheiser@example.com','Daija.Pacocha@example.com','Osvaldo.Bahringer@example.com','Halie.Heller@example.com','Chauncey.Schiller@example.com','Oral.Gleason@example.com','Lupe.Hilll@example.com','Jocelyn.Grant@example.com','Hollie.Kunde@example.com','Ed.Olson@example.com','Jeromy.Larkin@example.com','Marquis.Oberbrunner@example.com','Lyda.Macejkovic@example.com','Gordon.Ullrich@example.com','Kenton.Kilback@example.com','Maya.DuBuque@example.com','Jeromy.Abernathy@example.com','Furman.Volkman@example.com','Yesenia.Schmeler@example.com','Waylon.Wiza@example.com','Helen.Trantow@example.com','Claudine.McKenzie@example.com','Korbin.Sawayn@example.com','Brionna.Mertz@example.com','Orin.Mraz@example.com','Rusty.Hickle@example.com','Rafaela.Harvey@example.com','Herminio.Kub@example.com','Lera.White@example.com','Chandler.Collier@example.com','Edd.Thompson@example.com');
