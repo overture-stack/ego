bec74ec6969a217a64e08475d2d78e166c0cca7e
Jon Eubank <joneubank@gmail.com>
2017-11-25 10:10:45 -0500

    Refactor User Event and Receiver Addresses two problems: 1. Event name wasn't unique, was reporting as "UPDATE". By adding the class name to the event identifier there won't be confusion between different update events. 2. The EventBus would break on dispatch of an event if the notify event had a different Object type than one of its subscribers. Some testing showed that this would prevent events with the matching data type from receiving the event - stopped dispatch completely. Careful coding would ensure all events had a data type to match the consumers, but this but the safer way to handle it is to make all Consumers of type Event<?> so that they will accept all Event payloads, and then to make the consumer explicitly cast the payload to the type they expect.
    Now that the consumers are safe to receive all Events, we want to make it straightforward for users of our Events to pass the expected data type. To do this, we moved the eventBus.notify code into a method in the UserEvent class. Now the invoker of the event doesn't need to know abotu eventBus or any reactor code - only needs to call UserEvents.update(user). This also ensures that the correct type of object is passed in the Event payload.

--- src/main/java/org/overture/ego/reactor/events/UserEvents.java
+++ src/main/java/org/overture/ego/reactor/events/UserEvents.java
@@ -1,5 +1,25 @@
 package org.overture.ego.reactor.events;
 
-public enum UserEvents {
-  UPDATE
+import org.overture.ego.model.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+
+@Service
+public class UserEvents {
+
+  // EVENT NAMES
+  public static String UPDATE = UserEvents.class.getName() + ".UPDATE";
+
+  @Autowired
+  private EventBus eventBus;
+
+  public void update(User user) {
+    eventBus.notify(
+      UserEvents.UPDATE.toString(),
+      Event.wrap(user)
+    );
+  }
+
 }
--- src/main/java/org/overture/ego/token/TokenService.java
+++ src/main/java/org/overture/ego/token/TokenService.java
@@ -26,8 +26,6 @@ import org.overture.ego.utils.Types;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import reactor.bus.Event;
-import reactor.bus.EventBus;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -45,10 +43,9 @@ public class TokenService {
   @Autowired
   UserService userService;
   @Autowired
-  SimpleDateFormat dateFormatter;
-
+  UserEvents userEvents;
   @Autowired
-  EventBus eventBus;
+  SimpleDateFormat dateFormatter;
 
   public String generateUserToken(IDToken idToken){
     // If the demo flag is set, all tokens will be generated as the Demo User,
@@ -65,12 +62,10 @@ public class TokenService {
     }
 
     // Update user.lastLogin in the DB
-    // Do this via eventBus (Non-blocking)
+    // Use events as these are async:
+    //    the DB call won't block returning the Token
     user.setLastLogin(dateFormatter.format(new Date()));
-    eventBus.notify(
-      UserEvents.UPDATE.toString(),
-      Event.wrap(user)
-    );
+    userEvents.update(user);
 
     return generateUserToken(new TokenUserInfo(user));
   }
