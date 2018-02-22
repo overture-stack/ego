a7c89862a06f2b4285580443918306ab033be07f
Jonathan Ganesh <jganesh@olm-loaner3858.local>
2018-02-22 11:55:46 -0500

    Correct IAM role

--- dataservice-api/deploy_stage/deploy.sh
+++ dataservice-api/deploy_stage/deploy.sh
@@ -8,11 +8,11 @@ if [ $1 = "dev" ]; then
   echo 'key        = "dev/kf-dev-pi-egoserverservice-us-east-1-RSF"' >> dev.conf
   terraform init -backend=true -backend-config=dev.conf
   terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfArrangerserviceApiRole"' -var 'application=egoserverservice-api' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfEgoserviceApiRole"' -var 'application=egoserverservice-api' \
    -var 'service_name="kf-api-egoserverservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
    -var 'vault_role="kf_egoserverservice_api_role"'
   terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfArrangerserviceApiRole"' -var 'application=egoserverservice-api' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfEgoserviceApiRole"' -var 'application=egoserverservice-api' \
    -var 'service_name="kf-api-egoserverservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
    -var 'vault_role="kf_egoserverservice_api_role"'
 fi
@@ -25,11 +25,11 @@ if [ $1 = "qa" ]; then
   echo 'key        = "qa/kf-qa-api-egoserverservice-us-east-1-RSF"' >> qa.conf
   terraform init -backend=true -backend-config=qa.conf
   terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfArrangerserviceApiRole"' -var 'application=egoserverservice-api' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfEgoserviceApiRole"' -var 'application=egoserverservice-api' \
    -var 'service_name="kf-api-egoserverservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
    -var 'vault_role="kf_egoserverservice_api_role"'
   terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfArrangerserviceApiRole"' -var 'application=egoserverservice-api' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfEgoserviceApiRole"' -var 'application=egoserverservice-api' \
    -var 'service_name="kf-api-egoserverservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
    -var 'vault_role="kf_egoserverservice_api_role"'
 fi
