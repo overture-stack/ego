e658ff217aa82ba5feea7226b42757603b1ca411
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-03-21 14:06:53 -0400

    Added Vault support

---
+++ src/main/resources/scripts/setup_vault.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./vault write secret/ego/dev spring.datasource.username=postgres spring.datasource.password='' facebook.client.id=140524976574963 facebook.client.secret=2439abe7ae008bda7ab5cfdf706b4d66 google.client.Ids=808545688838-99s198l9lhl2hsvkpo5u91f3sflegemp.apps.googleusercontent.com,911372380614-7m296bg4eadc7m43e2mm6fs1a0ggkke1.apps.googleusercontent.com,814606937527-v7tr5dfqegjijicq3jeu5arv5tcl4ks0.apps.googleusercontent.com,814606937527-kk7ooglk6pj2tvpn7ldip6g3b74f8o72.apps.googleusercontent.com token.key-alias=ego-jwt token.keystore-password:=eG0tistic@lly
+
