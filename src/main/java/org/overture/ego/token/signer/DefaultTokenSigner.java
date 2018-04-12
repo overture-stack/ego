e18f8225c05419328b0284d20e5f298ae01cc592
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-04-12 12:16:59 -0400

    Cleanup

--- src/main/java/org/overture/ego/token/signer/DefaultTokenSigner.java
+++ src/main/java/org/overture/ego/token/signer/DefaultTokenSigner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017. The Ontario Institute for Cancer Research. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.overture.ego.token.signer;
 
 import lombok.SneakyThrows;
@@ -5,16 +21,12 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Optional;
@@ -48,7 +60,7 @@ public class DefaultTokenSigner implements TokenSigner {
   @PostConstruct
   @SneakyThrows
   private void init(){
-    keyFactory = KeyFactory.getInstance("RSA");
+    keyFactory = KeyFactory.getInstance(KEYFACTORY_TYPE);
     try {
       val decodedpriv = Base64.getDecoder().decode(encodedPrivKey);
       val decodedPub =  Base64.getDecoder().decode(encodedPubKey);
