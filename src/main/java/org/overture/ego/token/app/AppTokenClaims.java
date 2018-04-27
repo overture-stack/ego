369f3107ee0d5d737209e89a8fd748e8c4d525e5
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-04-27 01:44:27 -0400

    Cleanup

--- src/main/java/org/overture/ego/token/app/AppTokenClaims.java
+++ src/main/java/org/overture/ego/token/app/AppTokenClaims.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017. The Ontario Institute for Cancer Research. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.overture.ego.token.app;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -7,7 +23,6 @@ import lombok.NonNull;
 import org.overture.ego.token.TokenClaims;
 import org.overture.ego.view.Views;
 import org.springframework.util.StringUtils;
-
 import java.util.Arrays;
 import java.util.List;
 
