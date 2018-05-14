284e161e57d41704e860c5987191d1a22c555a76
Alex <lepsalex@gmail.com>
2018-05-14 11:51:10 -0400

    ApplicationServiceTest start

---
+++ src/test/java/org/overture/ego/service/ApplicationServiceTest.java
@@ -0,0 +1,22 @@
+package org.overture.ego.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationServiceTest {
+
+  @Before
+  void setup() {
+    
+  }
+
+  @Test
+  void testCreate() {
+
+  }
+}
