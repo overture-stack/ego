d34f863224d43844edb839fa7d3530e988c54bfd
Jon Eubank <joneubank@gmail.com>
2018-05-28 18:39:52 -0400

    removing sql used in demo

--- src/main/resources/dummy-data/temp-demo/02-insert-dummy-groups.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE public.egogroup CASCADE;
-
-INSERT INTO egogroup (name, status, description) VALUES ('AWG-KIDNEY', 'Approved', 'AWG Kidney pre-released data');
-INSERT INTO egogroup (name, status, description) VALUES ('AWG-BRAIN', 'Approved', 'AWG Brain pre-released data');
--- src/main/resources/dummy-data/temp-demo/03-insert-dummy-applications.sql
+++
@@ -1,8 +0,0 @@
-
-TRUNCATE public.egoapplication CASCADE;
-
-INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('TCGA-KIRC', 'tcga-kirc', 'tcga-kirc-secret', 'http://google.com', 'TCGA-KIRC', 'Approved');
-INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('TCGA-KIRP', 'tcga-kirp', 'tcga-kirp-secret', 'http://google.com', 'TCGA-KIRP', 'Approved');
-INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('TCGA-KICH', 'tcga-kich', 'tcga-kich-secret', 'http://google.com', 'TCGA-KICH', 'Approved');
-INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('TCGA-GBM',  'tcga-gbm', 'tcga-gbm-secret', 'http://google.com', 'TCGA-GBM', 'Approved');
-INSERT INTO EGOAPPLICATION (name, clientId, clientSecret, redirectUri, description, status) VALUES ('TCGA-LGG',  'tcga-lgg', 'tcga-lgg-secret', 'http://google.com', 'TCGA-LGG', 'Approved');
