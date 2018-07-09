11105d97f866cf7bd88e8faa58c40b4513c483c0
Alex <lepsalex@gmail.com>
2018-07-09 16:10:49 -0400

    Delete with passing test

--- src/main/java/org/overture/ego/service/AclEntityService.java
+++ src/main/java/org/overture/ego/service/AclEntityService.java
@@ -3,10 +3,16 @@ package org.overture.ego.service;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.overture.ego.model.entity.AclEntity;
+import org.overture.ego.model.search.SearchFilter;
 import org.overture.ego.repository.AclEntityRepository;
+import org.overture.ego.repository.queryspecification.AclEntitySpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -19,16 +25,23 @@ public class AclEntityService extends BaseService<AclEntity> {
   @Autowired
   private AclEntityRepository aclEntityRepository;
 
+  // Create
   public AclEntity create(@NonNull AclEntity aclEntity) {
     return aclEntityRepository.save(aclEntity);
   }
 
+
+  // Read
   public AclEntity get(@NonNull String aclEntityId) {
     return getById(aclEntityRepository, Integer.parseInt(aclEntityId));
   }
 
   public AclEntity getByName(@NonNull String aclEntityName) {
     return aclEntityRepository.findOneByNameIgnoreCase(aclEntityName);
+  }
+
+  public Page<AclEntity> listAclEntities(@NonNull List<SearchFilter> filters, @NonNull Pageable pageable) {
+    return aclEntityRepository.findAll(AclEntitySpecification.filterBy(filters), pageable);
   }
 
 
