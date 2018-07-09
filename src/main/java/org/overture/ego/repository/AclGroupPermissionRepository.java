09b68e15ba8463f3a17f0e0c37d7e2a29770e9f2
Alex <lepsalex@gmail.com>
2018-07-09 11:04:25 -0400

    Acl Repositories passed correct type

--- src/main/java/org/overture/ego/repository/AclGroupPermissionRepository.java
+++ src/main/java/org/overture/ego/repository/AclGroupPermissionRepository.java
@@ -1,9 +1,9 @@
 package org.overture.ego.repository;
 
-import org.overture.ego.model.entity.Application;
+import org.overture.ego.model.entity.AclGroupPermission;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface AclGroupPermissionRepository
-    extends PagingAndSortingRepository<Application, Integer>, JpaSpecificationExecutor {
+    extends PagingAndSortingRepository<AclGroupPermission, Integer>, JpaSpecificationExecutor {
 }
--- src/main/java/org/overture/ego/repository/AclUserPermissionRepository.java
+++ src/main/java/org/overture/ego/repository/AclUserPermissionRepository.java
@@ -1,9 +1,9 @@
 package org.overture.ego.repository;
 
-import org.overture.ego.model.entity.Application;
+import org.overture.ego.model.entity.AclUserPermission;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface AclUserPermissionRepository
-    extends PagingAndSortingRepository<Application, Integer>, JpaSpecificationExecutor {
+    extends PagingAndSortingRepository<AclUserPermission, Integer>, JpaSpecificationExecutor {
 }
