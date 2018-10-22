c0c33e225d6b2ef82bc6a0048974fb52a4c1063e
khartmann <kevin.hartmann@oicr.on.ca>
2018-10-22 18:44:39 -0400

    Code Cleanup: Moved code for checkToken() from the TokenController to the TokenService. Bugfix: checkToken() now limits the scope of the token so that it only includes scopes from the token that are legal scopes of the owner of the token when the check is done. This lets us revoke tokens as necessary. Bugfix: checkToken() now correctly validates the client id given in the the authorization token against the application restriction list specified by the token, if it is exists(ie. not null and not empty). Enhancement: Added many helper methods to EntityGenerator.java to set up entities. Enhancement: We now run unit tests for checkToken() and issueToken()

--- src/test/java/org/overture/ego/token/TokenServiceTest.java
+++ src/test/java/org/overture/ego/token/TokenServiceTest.java
@@ -20,6 +20,7 @@ package org.overture.ego.token;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,13 +28,23 @@ import org.overture.ego.service.ApplicationService;
 import org.overture.ego.service.GroupService;
 import org.overture.ego.service.UserService;
 import org.overture.ego.utils.EntityGenerator;
+import org.overture.ego.utils.TestData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.util.Pair;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.common.exceptions.InvalidScopeException;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import javax.management.InvalidApplicationException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
 
 @Slf4j
 @SpringBootTest
@@ -56,6 +67,16 @@ public class TokenServiceTest {
   @Autowired
   private TokenService tokenService;
 
+  public static TestData test;
+
+
+  @Before
+  public void initDb() {
+    if (test == null) {
+      test = new TestData(entityGenerator);
+    }
+  }
+
   @Test
   public void generateUserToken() {
     val user = userService.create(entityGenerator.createOneUser(Pair.of("foo", "bar")));
@@ -70,9 +91,212 @@ public class TokenServiceTest {
     app2.setWholeUsers(Sets.newHashSet(user));
     applicationService.update(app2);
 
-
     val token = tokenService.generateUserToken(userService.get(user.getId().toString()));
     assertNotNull(token);
   }
 
+  @Test
+  public void checkTokenWithExcessiveScopes() {
+    // Create a token for a user who issued the token having had the
+    // full set of scopes for the token, but now no longer does.
+    //
+    // We should get back only those scopes that are both in the token and that
+    // the user still has.
+    //
+    val tokenString = "491044a1-3ffd-4164-a6a0-0e1e666b28dc";
+    val scopes = test.policies("song.upload", "id.create", "collab.upload", "collab.download");
+    entityGenerator.setupToken(test.user2, tokenString,1000, scopes,null);
+    val result = tokenService.checkToken(test.scoreAuth, tokenString);
+
+    System.err.println(test.user2.getPermissions());
+    assertEquals(test.scoreId, result.getClient_id() );
+    assertTrue(result.getExp() > 900);
+    assertEquals(setOf("song.upload"), result.getScope());
+    assertEquals(test.user2.getName(), result.getUser_name());
+  }
+
+  @Test
+  public void checkTokenWithEmptyAppsList() {
+    // Check a valid token for a user, with an empty application restriction list.
+    // We should get back all the scopes that we set for our token.
+
+    val tokenString = "591044a1-3ffd-4164-a6a0-0e1e666b28dc";
+    val scopes = test.policies("song.upload", "song.download");
+    entityGenerator.setupToken(test.user1, tokenString,1000, scopes,null);
+
+    val result = tokenService.checkToken(test.songAuth, tokenString);
+
+    assertEquals(test.songId, result.getClient_id() );
+    assertTrue(result.getExp() > 900);
+    assertEquals(setOf("song.upload", "song.download"), result.getScope());
+    assertEquals(test.user1.getName(), result.getUser_name());
+  }
+
+  @Test
+  public void checkTokenWithWrongAuthToken() {
+    // Create a token with an application restriction list
+    // ("score"), and then try to check it with an authentication
+    // token for an application("song") that isn't on the token's
+    // application list.
+    //
+    // check_token should fail with an InvalidToken exception.
+    val tokenString = "691044a1-3ffd-4164-a6a0-0e1e666b28dc";
+    val scopes = test.policies("song.upload", "song.download");
+    val applications = Collections.singleton(test.score);
+    entityGenerator.setupToken(test.user1, tokenString,1000, scopes,applications);
+
+    InvalidTokenException ex=null;
+    try {
+      tokenService.checkToken(test.songAuth, tokenString);
+    } catch (InvalidTokenException e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+  }
+
+  @Test
+  public void checkTokenWithRightAuthToken() {
+    // Create a token with an application restriction list
+    // ("score"), and then try to check it with the same
+    // auth token.
+    //
+    // We should get back the values we sent.
+    val tokenString = "791044a1-3ffd-4164-a6a0-0e1e666b28dc";
+
+    val scopes = test.policies("song.upload", "song.download");
+    val applications = Collections.singleton(test.score);
+    entityGenerator.setupToken(test.user1, tokenString,1000, scopes,applications);
+
+    val result = tokenService.checkToken(test.scoreAuth, tokenString);
+
+    assertEquals(test.scoreId, result.getClient_id());
+    assertTrue( result.getExp() > 900);
+    assertEquals(setOf("song.upload", "song.download"), result.getScope());
+    assertEquals(test.user1.getName(), result.getUser_name());
+  }
+
+  @Test
+  public void checkTokenNullToken() {
+    // check_token() should fail with an InvalidTokenException
+    // if we pass it a null value for a token.
+
+    InvalidTokenException ex=null;
+    try {
+      tokenService.checkToken(test.songAuth, null);
+    } catch (InvalidTokenException e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+  }
+
+  @Test
+  public void checkTokenDoesNotExist() {
+    // check_token() should fail if we pass it a value for a
+    // token that we can't find.
+    InvalidTokenException ex=null;
+    try {
+      tokenService.checkToken(test.songAuth, "fakeToken");
+    } catch (InvalidTokenException e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+  }
+
+  @Test
+  public void issueTokenForInvalidUser() {
+    // Try to issue a token for a user that does not exist
+    val name="Invalid";
+    val scopes = setOf("collab.upload", "collab.download");
+    val applications = setOf("song", "score");
+
+    UsernameNotFoundException ex=null;
+    try {
+      tokenService.issueToken(name, scopes, applications);
+    } catch (UsernameNotFoundException e) {
+      ex = e;
+    }
+
+    assertNotNull(ex);
+  }
+
+  @Test
+  public void issueTokenWithExcessiveScope() {
+    // Try to issue a token for a user that exists, but with scopes that the user
+    // does not have access to.
+    //
+    // issueToken() should throw an InvalidScope exception
+    val name = test.user2.getName();
+    val scopes = setOf("collab.upload", "collab.download");
+    val applications = setOf();
+
+    InvalidScopeException ex=null;
+
+    try {
+      tokenService.issueToken(name, scopes, applications);
+    } catch (InvalidScopeException e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+
+  }
+  @Test
+  public void issueTokenForLimitedScopes() {
+    // Issue a token for a subset of the scopes the user has.
+    //
+    // issue_token() should return a token with values we set.
+    val name = test.user1.getName();
+    val scopes = setOf("collab.upload", "collab.download");
+    val applications = setOf();
+
+    val token = tokenService.issueToken(name, scopes, applications);
+
+    assertFalse(token.isRevoked());
+    assertEquals(token.getOwner().getId(), test.user1.getId());
+    assertTrue(token.getScope().equals(scopes));
+  }
+
+  @Test
+  public void issueTokenForInvalidScope() {
+    // Issue a token for a scope that does not exist.
+    //
+    // issue_token() should throw an exception
+
+    val name = test.user1.getName();
+    val scopes = setOf("collab.download", "collab.offload");
+    val applications = setOf();
+
+    InvalidScopeException ex=null;
+
+    try {
+      tokenService.issueToken(name, scopes, applications);
+    } catch (InvalidScopeException e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+  }
+
+  @Test
+  public void issueTokenForInvalidApp() {
+    // Issue a token for an application that does not exist.
+    //
+    // issue_token() should throw an exception
+
+    val name = test.user1.getName();
+    val scopes = setOf("collab.download", "id.create");
+    val applications = setOf("NotAnApplication");
+
+    Exception ex=null;
+
+    try {
+      tokenService.issueToken(name, scopes, applications);
+    } catch (Exception e) {
+      ex = e;
+    }
+    assertNotNull(ex);
+    assertTrue(ex instanceof InvalidApplicationException);
+  }
+
+  private static Set<String> setOf(String... strings) {
+    return new HashSet<>(Arrays.asList(strings));
+  }
 }
