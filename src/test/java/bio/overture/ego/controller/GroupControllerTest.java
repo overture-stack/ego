93a39b1086d9a2aa890dbde57bd6edd48bf4f0d2
Alex <lepsalex@gmail.com>
2018-12-10 14:46:21 -0500

    More complete delete test and todos for future tests

--- src/test/java/bio/overture/ego/controller/GroupControllerTest.java
+++ src/test/java/bio/overture/ego/controller/GroupControllerTest.java
@@ -3,12 +3,12 @@ package bio.overture.ego.controller;
 import bio.overture.ego.AuthorizationServiceMain;
 import bio.overture.ego.model.entity.Group;
 import bio.overture.ego.service.GroupService;
+import bio.overture.ego.service.UserService;
 import bio.overture.ego.utils.EntityGenerator;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.json.JSONException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +19,21 @@ import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
+import static bio.overture.ego.utils.CollectionUtils.listOf;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_ARRAY_ITEMS;
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @Slf4j
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = AuthorizationServiceMain.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+    classes = AuthorizationServiceMain.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GroupControllerTest {
 
   @LocalServerPort private int port;
@@ -38,6 +44,7 @@ public class GroupControllerTest {
 
   @Autowired private EntityGenerator entityGenerator;
   @Autowired private GroupService groupService;
+  @Autowired private UserService userService;
 
   @Before
   public void Setup() {
@@ -61,13 +68,26 @@ public class GroupControllerTest {
 
     HttpEntity<Group> entity = new HttpEntity<Group>(group, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort("/groups"),
-        HttpMethod.POST, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(createURLWithPort("/groups"), HttpMethod.POST, entity, String.class);
 
     HttpStatus responseStatus = response.getStatusCode();
+    assertEquals(HttpStatus.OK, responseStatus);
+  }
 
-    assertEquals(responseStatus, HttpStatus.OK);
+  @Test
+  public void AddUniqueGroup() {
+
+    entityGenerator.setupGroup("SameSame");
+    Group group = new Group("SameSame");
+
+    HttpEntity<Group> entity = new HttpEntity<Group>(group, headers);
+
+    ResponseEntity<String> response =
+        restTemplate.exchange(createURLWithPort("/groups"), HttpMethod.POST, entity, String.class);
+
+    HttpStatus responseStatus = response.getStatusCode();
+    assertEquals(HttpStatus.CONFLICT, responseStatus); // TODO
   }
 
   @Test
@@ -78,40 +98,65 @@ public class GroupControllerTest {
 
     HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort(String.format("/groups/%s", groupId)),
-        HttpMethod.GET, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/groups/%s", groupId)),
+            HttpMethod.GET,
+            entity,
+            String.class);
 
     HttpStatus responseStatus = response.getStatusCode();
     String responseBody = response.getBody();
 
-    String expected = String.format("{\"id\":\"%s\",\"name\":\"Group One\",\"description\":null,\"status\":null}", groupId);
+    String expected =
+        String.format(
+            "{\"id\":\"%s\",\"name\":\"Group One\",\"description\":null,\"status\":null}", groupId);
 
-    assertEquals(responseStatus, HttpStatus.OK);
+    assertEquals(HttpStatus.OK, responseStatus);
     assertThatJson(responseBody).isEqualTo(expected);
+  }
+
+  @Test
+  public void GetGroupNotFound() throws JSONException {
+    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+    ResponseEntity<String> response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/groups/%s", UUID.randomUUID())),
+            HttpMethod.GET,
+            entity,
+            String.class);
+
+    HttpStatus responseStatus = response.getStatusCode();
+
+    assertEquals(HttpStatus.NOT_FOUND, responseStatus); // TODO
   }
 
   @Test
   public void ListGroups() throws JSONException {
     HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort("/groups"),
-        HttpMethod.GET, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(createURLWithPort("/groups"), HttpMethod.GET, entity, String.class);
 
     HttpStatus responseStatus = response.getStatusCode();
     String responseBody = response.getBody();
 
-    String expected = String.format(
-        "[{\"id\":\"%s\",\"name\":\"Group One\",\"description\":null,\"status\":null}, {\"id\":\"%s\",\"name\":\"Group Two\",\"description\":null,\"status\":null}, {\"id\":\"%s\",\"name\":\"Group Three\",\"description\":null,\"status\":null}]",
-        groupService.getByName("Group One").getId(),
-        groupService.getByName("Group Two").getId(),
-        groupService.getByName("Group Three").getId()
-    );
+    String expected =
+        String.format(
+            "[{\"id\":\"%s\",\"name\":\"Group One\",\"description\":null,\"status\":null}, {\"id\":\"%s\",\"name\":\"Group Two\",\"description\":null,\"status\":null}, {\"id\":\"%s\",\"name\":\"Group Three\",\"description\":null,\"status\":null}]",
+            groupService.getByName("Group One").getId(),
+            groupService.getByName("Group Two").getId(),
+            groupService.getByName("Group Three").getId());
 
     assertEquals(HttpStatus.OK, responseStatus);
-    assertThatJson(responseBody).when(IGNORING_EXTRA_ARRAY_ITEMS, IGNORING_ARRAY_ORDER).node("resultSet").isEqualTo(expected);
+    assertThatJson(responseBody)
+        .when(IGNORING_EXTRA_ARRAY_ITEMS, IGNORING_ARRAY_ORDER)
+        .node("resultSet")
+        .isEqualTo(expected);
   }
+
+  // TODO - ADD List/Filter tests
 
   @Test
   public void UpdateGroup() {
@@ -124,21 +169,24 @@ public class GroupControllerTest {
 
     HttpEntity<Group> entity = new HttpEntity<Group>(update, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort(String.format("/groups/%s", groupId)),
-        HttpMethod.PUT, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/groups/%s", groupId)),
+            HttpMethod.PUT,
+            entity,
+            String.class);
 
     String responseBody = response.getBody();
 
     HttpStatus responseStatus = response.getStatusCode();
-    assertEquals(responseStatus, HttpStatus.OK);
+    assertEquals(HttpStatus.OK, responseStatus);
     assertThatJson(responseBody).node("id").isEqualTo(groupId);
     assertThatJson(responseBody).node("name").isEqualTo("Updated Complete");
   }
 
+  // TODO - ADD Update non-existent entity
+
   @Test
-  @Ignore
-  // TODO: Implement PATCH
   public void PartialUpdateGroup() throws JSONException {
 
     // Groups created in setup
@@ -147,14 +195,17 @@ public class GroupControllerTest {
     val update = "{\"name\":\"Updated Partial\"}";
     HttpEntity<String> entity = new HttpEntity<String>(update, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort(String.format("/groups/%s", groupId)),
-        HttpMethod.PATCH, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/groups/%s", groupId)),
+            HttpMethod.PATCH,
+            entity,
+            String.class); // TODO - No Patch Method
 
     String responseBody = response.getBody();
 
     HttpStatus responseStatus = response.getStatusCode();
-    assertEquals(responseStatus, HttpStatus.OK);
+    assertEquals(HttpStatus.OK, responseStatus);
     assertThatJson(responseBody).node("id").isEqualTo(groupId);
     assertThatJson(responseBody).node("name").isEqualTo("Updated Partial");
   }
@@ -165,20 +216,35 @@ public class GroupControllerTest {
     // Groups created in setup
     val groupId = entityGenerator.setupGroup("Temporary").getId();
 
+    // Add a user to this group
+    userService.addUserToGroups(
+        userService.getByName("FirstUser@domain.com").getId().toString(),
+        listOf(groupId.toString()));
+
+    // TODO - ADD application groups relationship
+
     HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
-    ResponseEntity<String> response = restTemplate.exchange(
-        createURLWithPort(String.format("/groups/%s", groupId)),
-        HttpMethod.DELETE, entity, String.class);
+    ResponseEntity<String> response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/groups/%s", groupId)),
+            HttpMethod.DELETE,
+            entity,
+            String.class);
 
     HttpStatus responseStatus = response.getStatusCode();
-    String responseBody = response.getBody();
 
-    String expected = String.format("{\"id\":\"%s\",\"name\":\"Group One\",\"description\":null,\"status\":null}", groupId);
+    // Check http response
+    assertEquals(HttpStatus.OK, responseStatus);
 
-    assertEquals(responseStatus, HttpStatus.OK);
+    // Check user-group relationship is also deleted
+    assertNotEquals(null, userService.getByName("FirstUser@domain.com"));
+
+    // Check group is deleted
     assertEquals(null, groupService.getByName("Temporary"));
   }
+
+  // TODO - ADD tests for adding user/apps to groups
 
   private String createURLWithPort(String uri) {
     return "http://localhost:" + port + uri;
