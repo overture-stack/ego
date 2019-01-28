0efe2163c1fb842abe513a1854c2533be4b92bc9
Dušan Andrić <Dusan.Andric@gmail.com>
2019-01-28 16:50:42 -0500

    more user tests, lombok, camelCase

--- src/test/java/bio/overture/ego/controller/UserControllerTest.java
+++ src/test/java/bio/overture/ego/controller/UserControllerTest.java
@@ -17,16 +17,22 @@
 
 package bio.overture.ego.controller;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import bio.overture.ego.AuthorizationServiceMain;
-import bio.overture.ego.model.entity.Group;
 import bio.overture.ego.model.entity.User;
 import bio.overture.ego.service.ApplicationService;
 import bio.overture.ego.service.GroupService;
 import bio.overture.ego.service.UserService;
 import bio.overture.ego.utils.EntityGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import java.util.stream.StreamSupport;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,6 +52,8 @@ import org.springframework.test.context.junit4.SpringRunner;
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
 
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
   @LocalServerPort private int port;
   private TestRestTemplate restTemplate = new TestRestTemplate();
   private HttpHeaders headers = new HttpHeaders();
@@ -58,7 +66,7 @@ public class UserControllerTest {
   @Autowired private ApplicationService applicationService;
 
   @Before
-  public void Setup() {
+  public void setup() {
 
     // Initial setup of entities (run once
     if (!hasRunEntitySetup) {
@@ -73,9 +81,9 @@ public class UserControllerTest {
   }
 
   @Test
-  public void AddUser() {
+  public void addUser() {
 
-    User user =
+    val user =
         User.builder()
             .firstName("foo")
             .lastName("bar")
@@ -85,54 +93,105 @@ public class UserControllerTest {
             .status("Approved")
             .build();
 
-    HttpEntity<User> entity = new HttpEntity<User>(user, headers);
+    val entity = new HttpEntity<User>(user, headers);
 
-    ResponseEntity<String> response =
+    val response =
         restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, entity, String.class);
 
-    HttpStatus responseStatus = response.getStatusCode();
+    val responseStatus = response.getStatusCode();
     assertThat(responseStatus).isEqualTo(HttpStatus.OK);
   }
 
   @Test
-  public void AddUniqueUser() {
-    User user1 =
-      User.builder()
-        .firstName("unique")
-        .lastName("unique")
-        .email("unique@unique.com")
-        .preferredLanguage("English")
-        .role("USER")
-        .status("Approved")
-        .build();
+  public void addUniqueUser() {
+    val user1 =
+        User.builder()
+            .firstName("unique")
+            .lastName("unique")
+            .email("unique@unique.com")
+            .preferredLanguage("English")
+            .role("USER")
+            .status("Approved")
+            .build();
+    val user2 =
+        User.builder()
+            .firstName("unique")
+            .lastName("unique")
+            .email("unique@unique.com")
+            .preferredLanguage("English")
+            .role("USER")
+            .status("Approved")
+            .build();
 
-    User user2 =
-      User.builder()
-        .firstName("unique")
-        .lastName("unique")
-        .email("unique@unique.com")
-        .preferredLanguage("English")
-        .role("USER")
-        .status("Approved")
-        .build();
+    val entity1 = new HttpEntity<User>(user1, headers);
+    val response1 =
+        restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, entity1, String.class);
+    val responseStatus1 = response1.getStatusCode();
 
-    HttpEntity<User> entity1 = new HttpEntity<User>(user1, headers);
-
-    ResponseEntity<String> response1 =
-      restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, entity1, String.class);
-
-    HttpStatus responseStatus1 = response1.getStatusCode();
     assertThat(responseStatus1).isEqualTo(HttpStatus.OK);
 
-    HttpEntity<User> entity2 = new HttpEntity<User>(user2, headers);
-
-    ResponseEntity<String> response2 =
-      restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, entity2, String.class);
-
     // Return a 409 conflict because email already exists for a registered user.
-    HttpStatus responseStatus2 = response2.getStatusCode();
+    val entity2 = new HttpEntity<User>(user2, headers);
+    val response2 =
+        restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, entity2, String.class);
+    val responseStatus2 = response2.getStatusCode();
     assertThat(responseStatus2).isEqualTo(HttpStatus.CONFLICT);
   }
+
+  @Test
+  @SneakyThrows
+  public void getUser() {
+
+    // Users created in setup
+    val userId = userService.getByName("FirstUser@domain.com").getId();
+    val entity = new HttpEntity<String>(null, headers);
+    val response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/users/%s", userId)),
+            HttpMethod.GET,
+            entity,
+            String.class);
+
+    val responseStatus = response.getStatusCode();
+    val responseJson = MAPPER.readTree(response.getBody());
+
+    assertThat(responseStatus).isEqualTo(HttpStatus.OK);
+    assertThat(responseJson.get("firstName").asText()).isEqualTo("First");
+    assertThat(responseJson.get("lastName").asText()).isEqualTo("User");
+    assertThat(responseJson.get("name").asText()).isEqualTo("FirstUser@domain.com");
+    assertThat(responseJson.get("preferredLanguage").asText()).isEqualTo("English");
+    assertThat(responseJson.get("status").asText()).isEqualTo("Approved");
+    assertThat(responseJson.get("id").asText()).isEqualTo(userId.toString());
+  }
+
+  @Test
+  @SneakyThrows
+  public void listUsersNoFilter() {
+    val entity = new HttpEntity<String>(null, headers);
+    val response =
+        restTemplate.exchange(createURLWithPort("/users/"), HttpMethod.GET, entity, String.class);
+
+    val responseStatus = response.getStatusCode();
+    val responseJson = MAPPER.readTree(response.getBody());
+
+    assertThat(responseStatus).isEqualTo(HttpStatus.OK);
+    assertThat(responseJson.get("count").asInt()).isGreaterThanOrEqualTo(3);
+    assertThat(responseJson.get("resultSet").isArray()).isTrue();
+
+    // Verify that the returned Users are the ones from the setup.
+    Iterable<JsonNode> resultSetIterable =
+        () -> ((ArrayNode) responseJson.get("resultSet")).iterator();
+    val userNames =
+        StreamSupport.stream(resultSetIterable.spliterator(), false)
+            .map(j -> j.get("name").asText())
+            .collect(toList());
+    assertThat(userNames)
+        .contains("FirstUser@domain.com", "SecondUser@domain.com", "ThirdUser@domain.com");
+  }
+
+  @Test
+  @SneakyThrows
+  public void listUsersWithFilter() {}
 
   private String createURLWithPort(String uri) {
     return "http://localhost:" + port + uri;
