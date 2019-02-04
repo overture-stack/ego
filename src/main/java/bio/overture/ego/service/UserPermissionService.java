60aef38b1f00fb7c164ca2392dae86babd861523
Dušan Andrić <Dusan.Andric@gmail.com>
2019-02-04 11:56:44 -0500

    finishes policy tests for user/groups, bug fixes included

--- src/main/java/bio/overture/ego/service/UserPermissionService.java
+++ src/main/java/bio/overture/ego/service/UserPermissionService.java
@@ -1,5 +1,7 @@
 package bio.overture.ego.service;
 
+import static bio.overture.ego.repository.queryspecification.UserPermissionSpecification.withPolicy;
+import static bio.overture.ego.repository.queryspecification.UserPermissionSpecification.withUser;
 import static bio.overture.ego.utils.CollectionUtils.mapToList;
 import static java.util.UUID.fromString;
 import static org.springframework.data.jpa.domain.Specifications.where;
@@ -7,9 +9,10 @@ import static org.springframework.data.jpa.domain.Specifications.where;
 import bio.overture.ego.model.dto.PolicyResponse;
 import bio.overture.ego.model.entity.UserPermission;
 import bio.overture.ego.repository.UserPermissionRepository;
-import bio.overture.ego.repository.queryspecification.UserPermissionSpecification;
 import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Service;
@@ -25,8 +28,22 @@ public class UserPermissionService extends AbstractPermissionService<UserPermiss
   }
 
   public List<UserPermission> findAllByPolicy(@NonNull String policyId) {
-    return getRepository()
-        .findAll(where(UserPermissionSpecification.withPolicy(fromString(policyId))));
+    return getRepository().findAll(where(withPolicy(fromString(policyId))));
+  }
+
+  @SneakyThrows
+  public UserPermission findByPolicyAndUser(@NonNull String policyId, @NonNull String userId) {
+    val opt =
+        getRepository()
+            .findOne(where(withPolicy(fromString(policyId))).and(withUser(fromString(userId))));
+
+    return (UserPermission)
+        opt.orElseThrow(() -> new EntityNotFoundException("Permission cannot be found."));
+  }
+
+  public void deleteByPolicyAndUser(@NonNull String policyId, @NonNull String userId) {
+    val perm = findByPolicyAndUser(policyId, userId);
+    delete(perm.getId());
   }
 
   public List<PolicyResponse> findByPolicy(@NonNull String policyId) {
--- src/test/java/bio/overture/ego/controller/PolicyControllerTest.java
+++ src/test/java/bio/overture/ego/controller/PolicyControllerTest.java
@@ -24,6 +24,7 @@ import bio.overture.ego.model.entity.Policy;
 import bio.overture.ego.service.PolicyService;
 import bio.overture.ego.utils.EntityGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -168,6 +169,124 @@ public class PolicyControllerTest {
     assertThat(getResponseStatus).isEqualTo(HttpStatus.OK);
     assertThat(groupPermissionJson.get("id").asText()).isEqualTo(groupId);
     assertThat(groupPermissionJson.get("mask").asText()).isEqualTo("WRITE");
+  }
+
+  @Test
+  @SneakyThrows
+  public void deleteGroupPermission() {
+    val policyId = entityGenerator.setupSinglePolicy("DeleteGroupPermission").getId().toString();
+    val groupId = entityGenerator.setupGroup("GroupPolicyDelete").getId().toString();
+
+    val entity = new HttpEntity<String>("WRITE", headers);
+    val response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/permission/group/%s", policyId, groupId)),
+            HttpMethod.POST,
+            entity,
+            String.class);
+
+    val responseStatus = response.getStatusCode();
+    assertThat(responseStatus).isEqualTo(HttpStatus.OK);
+    // TODO: Fix it so that POST returns JSON, not just random string message
+
+    val deleteResponse =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/permission/group/%s", policyId, groupId)),
+            HttpMethod.DELETE,
+            new HttpEntity<String>(null, headers),
+            String.class);
+
+    val deleteResponseStatus = deleteResponse.getStatusCode();
+    assertThat(deleteResponseStatus).isEqualTo(HttpStatus.OK);
+
+    val getResponse =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/groups", policyId)),
+            HttpMethod.GET,
+            new HttpEntity<String>(null, headers),
+            String.class);
+
+    val getResponseStatus = getResponse.getStatusCode();
+    val getResponseJson = (ArrayNode) MAPPER.readTree(getResponse.getBody());
+
+    assertThat(getResponseStatus).isEqualTo(HttpStatus.OK);
+    assertThat(getResponseJson.size()).isEqualTo(0);
+  }
+
+  @Test
+  @SneakyThrows
+  public void addUserPermission() {
+    val policyId = entityGenerator.setupSinglePolicy("AddUserPermission").getId().toString();
+    val userId = entityGenerator.setupUser("UserPolicy Add").getId().toString();
+
+    val entity = new HttpEntity<String>("READ", headers);
+    val response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/permission/user/%s", policyId, userId)),
+            HttpMethod.POST,
+            entity,
+            String.class);
+
+    val responseStatus = response.getStatusCode();
+    assertThat(responseStatus).isEqualTo(HttpStatus.OK);
+    // TODO: Fix it so that POST returns JSON, not just random string message
+
+    val getResponse =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/users", policyId)),
+            HttpMethod.GET,
+            new HttpEntity<String>(null, headers),
+            String.class);
+
+    val getResponseStatus = getResponse.getStatusCode();
+    val getResponseJson = MAPPER.readTree(getResponse.getBody());
+    val groupPermissionJson = getResponseJson.get(0);
+
+    assertThat(getResponseStatus).isEqualTo(HttpStatus.OK);
+    assertThat(groupPermissionJson.get("id").asText()).isEqualTo(userId);
+    assertThat(groupPermissionJson.get("mask").asText()).isEqualTo("READ");
+  }
+
+  @Test
+  @SneakyThrows
+  public void deleteUserPermission() {
+    val policyId = entityGenerator.setupSinglePolicy("DeleteGroupPermission").getId().toString();
+    val userId = entityGenerator.setupUser("UserPolicy Delete").getId().toString();
+
+    val entity = new HttpEntity<String>("WRITE", headers);
+    val response =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/permission/user/%s", policyId, userId)),
+            HttpMethod.POST,
+            entity,
+            String.class);
+
+    val responseStatus = response.getStatusCode();
+    assertThat(responseStatus).isEqualTo(HttpStatus.OK);
+    // TODO: Fix it so that POST returns JSON, not just random string message
+
+    val deleteResponse =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/permission/user/%s", policyId, userId)),
+            HttpMethod.DELETE,
+            new HttpEntity<String>(null, headers),
+            String.class);
+
+    val deleteResponseStatus = deleteResponse.getStatusCode();
+    assertThat(deleteResponseStatus).isEqualTo(HttpStatus.OK);
+
+    val getResponse =
+        restTemplate.exchange(
+            createURLWithPort(String.format("/policies/%s/users", policyId)),
+            HttpMethod.GET,
+            new HttpEntity<String>(null, headers),
+            String.class);
+
+    val getResponseStatus = getResponse.getStatusCode();
+    val getResponseJson = (ArrayNode) MAPPER.readTree(getResponse.getBody());
+
+    assertThat(getResponseStatus).isEqualTo(HttpStatus.OK);
+    assertThat(getResponseJson.size()).isEqualTo(0);
   }
 
   private String createURLWithPort(String uri) {
