abf904ecc1e40ba21a89d65f562f4e856e8e6ee2
Jon Eubank <joneubank@gmail.com>
2019-04-09 13:54:27 -0400

    :bug: Fix group dummy data scripts to match latest schema

--- src/main/resources/dummy-data/02-insert-dummy-groups.sql
+++ src/main/resources/dummy-data/02-insert-dummy-groups.sql
@@ -1,11 +1,11 @@
 /* WARNING: Clears all data in the EgoGroup Table
 
-Clears the EgoGroup table and insert 5 sample groups (4 Approved, 1 Pending)
+Clears the EgoGroup table and insert 5 sample groups (4 APPROVED, 1 PENDING)
 */
 TRUNCATE public.egogroup CASCADE;
 
-INSERT INTO egogroup (name, status, description) VALUES ('XYZ Cancer Research Institute', 'Approved', 'Sample group for elite cancer researchers');
-INSERT INTO egogroup (name, status, description) VALUES ('Extreme Research Consortium', 'Approved', 'Sample group for generalist researchers');
-INSERT INTO egogroup (name, status, description) VALUES ('Healthcare Providers Anonymous', 'Approved', 'Sample group for patient care specialist');
-INSERT INTO egogroup (name, status, description) VALUES ('Pediatric Patient Support Network', 'Approved', 'Sample group for patients and their supporters');
-INSERT INTO egogroup (name, status, description) VALUES ('Generic Genomics Geniuses', 'Pending', 'Sample group for super-duper smart genetic investigators');
+INSERT INTO egogroup (name, status, description) VALUES ('XYZ Cancer Research Institute', 'APPROVED', 'Sample group for elite cancer researchers');
+INSERT INTO egogroup (name, status, description) VALUES ('Extreme Research Consortium', 'APPROVED', 'Sample group for generalist researchers');
+INSERT INTO egogroup (name, status, description) VALUES ('Healthcare Providers Anonymous', 'APPROVED', 'Sample group for patient care specialist');
+INSERT INTO egogroup (name, status, description) VALUES ('Pediatric Patient Support Network', 'APPROVED', 'Sample group for patients and their supporters');
+INSERT INTO egogroup (name, status, description) VALUES ('Generic Genomics Geniuses', 'PENDING', 'Sample group for super-duper smart genetic investigators');
