bd1a5ee5e90c924755975cd4102028e5a74c72d2
Jon Eubank <joneubank@gmail.com>
2019-04-09 10:43:51 -0400

    :truck: Organize gRPC services into sub package

--- src/main/java/bio/overture/ego/grpc/service/UserServiceImpl.java
+++ src/main/java/bio/overture/ego/grpc/service/UserServiceImpl.java
@@ -1,17 +1,14 @@
-package bio.overture.ego.grpc;
+package bio.overture.ego.grpc.service;
 
+import bio.overture.ego.grpc.*;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
-
-  @Autowired
-  public UserServiceImpl() {}
 
   @Override
   public void get(GetUserRequest request, StreamObserver<User> responseObserver) {
