c7eb510e80ad263bcc44d6b48652e478568fd349
Robert Tisma <rtisma@gmail.com>
2019-04-11 13:55:11 -0400

    add a generic service call to list entities and get many while also defining the fetch plan at runtime

--- src/main/java/bio/overture/ego/repository/queryspecification/builder/AbstractSpecificationBuilder.java
+++ src/main/java/bio/overture/ego/repository/queryspecification/builder/AbstractSpecificationBuilder.java
@@ -1,17 +1,18 @@
 package bio.overture.ego.repository.queryspecification.builder;
 
-import static bio.overture.ego.model.enums.JavaFields.NAME;
-
-import bio.overture.ego.model.entity.Identifiable;
 import bio.overture.ego.model.enums.JavaFields;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import lombok.NonNull;
 import lombok.val;
 import org.springframework.data.jpa.domain.Specification;
 
-public abstract class AbstractSpecificationBuilder<T extends Identifiable<ID>, ID> {
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+
+import static bio.overture.ego.model.enums.JavaFields.NAME;
+
+public abstract class AbstractSpecificationBuilder<T, ID> {
 
   protected abstract Root<T> setupFetchStrategy(Root<T> root);
 
@@ -22,11 +23,29 @@ public abstract class AbstractSpecificationBuilder<T extends Identifiable<ID>, I
     };
   }
 
+  public Specification<T> listAll(){
+    return (fromUser, query, builder) -> {
+      val root = setupFetchStrategy(fromUser);
+      return builder.isNotNull(root.get(JavaFields.ID));
+    };
+  }
+
   public Specification<T> buildById(@NonNull ID id) {
     return (fromUser, query, builder) -> {
       val root = setupFetchStrategy(fromUser);
       return equalsIdPredicate(root, builder, id);
     };
+  }
+
+  public Specification<T> buildByIds(@NonNull Collection<ID> ids) {
+    return (fromUser, query, builder) -> {
+      val root = setupFetchStrategy(fromUser);
+      return whereInIdsPredicate(root, ids);
+    };
+  }
+
+  private Predicate whereInIdsPredicate(Root<T> root, Collection<ID> ids) {
+    return root.get(JavaFields.ID).in(ids);
   }
 
   private Predicate equalsIdPredicate(Root<T> root, CriteriaBuilder builder, ID id) {
