b71ed9d2d3ffad45990231835776b27bf9bc5451
Jon Eubank <joneubank@gmail.com>
2019-04-17 17:10:05 -0400

    :sparkles: Added ProtoUtils
    StringValue easy conversion, Protobuf to Pageable conversion

---
+++ src/main/java/bio/overture/ego/grpc/ProtoUtils.java
@@ -0,0 +1,69 @@
+package bio.overture.ego.grpc;
+
+import com.google.protobuf.StringValue;
+import lombok.val;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public class ProtoUtils {
+
+  public static final StringValue DEFAULT_STRING = StringValue.of("");
+
+  /* toProtoString
+   * Use this to convert a potentially null value into a String for a protobuf value
+   *
+   * Will return Empty String if the value is null, otherwise the toString output for the Object
+   */
+  public static StringValue toProtoString(Object value) {
+    return value == null ? DEFAULT_STRING : StringValue.of(value.toString());
+  }
+
+  public static Pageable getPageable(PagedRequest pagedRequest) {
+    val limit = pagedRequest.getPageSize();
+    val pageNumber = pagedRequest.getPageNumber();
+    return new Pageable() {
+      private final int DEFAULT_LIMIT = 20;
+
+      @Override
+      public int getPageNumber() {
+        return 0;
+      }
+
+      @Override
+      public int getPageSize() {
+        return limit == 0 ? DEFAULT_LIMIT : limit;
+      }
+
+      @Override
+      public long getOffset() {
+        return pageNumber;
+      }
+
+      @Override
+      public Sort getSort() {
+        // Sort results by creation time, ensure static order for the page_token to refer to
+        return new Sort(Sort.Direction.ASC, "createdat");
+      }
+
+      @Override
+      public Pageable next() {
+        return null;
+      }
+
+      @Override
+      public Pageable previousOrFirst() {
+        return null;
+      }
+
+      @Override
+      public Pageable first() {
+        return null;
+      }
+
+      @Override
+      public boolean hasPrevious() {
+        return false;
+      }
+    };
+  }
+}
