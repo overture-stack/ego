548751b34020b6b28cfd4343dcb5c4572eb4ac5d
Jon Eubank <joneubank@gmail.com>
2019-04-17 17:10:37 -0400

    :sparkles: Add to User entity: toProto and fromProto

--- src/main/java/bio/overture/ego/model/entity/User.java
+++ src/main/java/bio/overture/ego/model/entity/User.java
@@ -16,8 +16,10 @@
 
 package bio.overture.ego.model.entity;
 
+import static bio.overture.ego.grpc.ProtoUtils.toProtoString;
 import static bio.overture.ego.model.enums.AccessLevel.EGO_ENUM;
 import static bio.overture.ego.service.UserService.resolveUsersPermissions;
+import static bio.overture.ego.utils.CollectionUtils.mapToImmutableSet;
 import static bio.overture.ego.utils.PolicyPermissionUtils.extractPermissionStrings;
 import static com.google.common.collect.Sets.newHashSet;
 
@@ -34,7 +36,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.google.protobuf.StringValue;
 import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -53,13 +58,9 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.LazyInitializationException;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
@@ -198,4 +199,136 @@ public class User implements PolicyOwner, NameableEntity<UUID> {
   public List<String> getPermissions() {
     return extractPermissionStrings(resolveUsersPermissions(this));
   }
+
+  /* ===========================
+   * Protobuf Conversion Methods
+   * =========================== */
+
+  public bio.overture.ego.grpc.User toProto() {
+
+    val builder =
+        bio.overture.ego.grpc.User.newBuilder()
+            .setId(toProtoString(this.getId()))
+            .setEmail(toProtoString(this.getEmail()))
+            .setFirstName(toProtoString(this.getFirstName()))
+            .setLastName(toProtoString(this.getLastName()))
+            .setName(toProtoString(this.getName()))
+            .setCreatedAt(toProtoString(this.getCreatedAt()))
+            .setLastLogin(toProtoString(this.getLastLogin()))
+            .setPreferredLanguage(toProtoString(this.getPreferredLanguage()))
+            .setStatus(toProtoString(this.getStatus()))
+            .setType(toProtoString(this.getType()));
+
+    try {
+      final Set<StringValue> applications =
+          mapToImmutableSet(
+              this.getApplications(), application -> toProtoString(application.getId()));
+      builder.addAllApplications(applications);
+
+    } catch (LazyInitializationException e) {
+      log.error(
+          "Could not add applications to gRPC User Object, cannot lazy load values:",
+          e.getMessage());
+    }
+
+    try {
+      final Set<StringValue> groups =
+          mapToImmutableSet(this.getUserGroups(), group -> toProtoString(group.getGroup().getId()));
+      builder.addAllGroups(groups);
+
+    } catch (LazyInitializationException e) {
+      log.error(
+          "Could not add groups to gRPC User Object, cannot lazy load values:", e.getMessage());
+    }
+
+    try {
+      final Set<StringValue> permissions =
+          mapToImmutableSet(
+              this.getUserPermissions(),
+              permission -> toProtoString(permission.getPolicy().getName()));
+      builder.addAllPermissions(permissions);
+
+    } catch (LazyInitializationException e) {
+      log.error(
+          "Could not add permissions to gRPC User Object, cannot lazy load values:",
+          e.getMessage());
+    }
+
+    return builder.build();
+  }
+
+  public static User fromProto(bio.overture.ego.grpc.User proto) {
+    val user = new User();
+
+    if (proto.hasEmail()) {
+      user.setEmail(proto.getEmail().getValue());
+    }
+
+    if (proto.hasFirstName()) {
+      user.setFirstName(proto.getFirstName().getValue());
+    }
+
+    if (proto.hasLastName()) {
+      user.setLastName(proto.getLastName().getValue());
+    }
+
+    if (proto.hasName()) {
+      user.setName(proto.getName().getValue());
+    }
+
+    try {
+      if (proto.hasCreatedAt()) {
+        final String date = proto.getCreatedAt().getValue();
+        if (!date.isEmpty()) {
+          user.setCreatedAt(dateFormat.parse(date));
+        }
+      }
+    } catch (ParseException e) {
+      log.debug("Could not parse created date from protobuf: ", e.getMessage());
+    }
+
+    try {
+      if (proto.hasLastLogin()) {
+        final String date = proto.getLastLogin().getValue();
+        if (!date.isEmpty()) {
+          user.setLastLogin(dateFormat.parse(date));
+        }
+      }
+    } catch (ParseException e) {
+      log.debug("Could not parse last login date from protobuf: ", e.getMessage());
+    }
+
+    try {
+      if (proto.hasPreferredLanguage()) {
+        final String preferredLanguage = proto.getPreferredLanguage().getValue();
+        user.setPreferredLanguage(LanguageType.valueOf(preferredLanguage));
+      }
+    } catch (IllegalArgumentException e) {
+      log.debug("Could not set preferred language from protobuf: ", e.getMessage());
+    }
+
+    try {
+      if (proto.hasStatus()) {
+
+        final String status = proto.getStatus().getValue();
+        user.setStatus(StatusType.resolveStatusType(status));
+      }
+    } catch (IllegalArgumentException e) {
+      log.debug("Could not set status from protobuf: ", e.getMessage());
+    }
+
+    try {
+      if (proto.hasType()) {
+        final String userType = proto.getType().getValue();
+        user.setType(UserType.resolveUserType(userType));
+      }
+
+    } catch (IllegalArgumentException e) {
+      log.debug("Could not set user type from protobuf: ", e.getMessage());
+    }
+
+    return user;
+  }
+
+  private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 }
