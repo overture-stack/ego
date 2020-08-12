fb862581e5e9905b17ba7e8a227312a6b05d6bdd
Ann Catton <ann.catton@oicr.on.ca>
2020-08-12 18:32:55 -0400

    Override default parameters in oauth token-endpoint

--- src/main/java/bio/overture/ego/config/SwaggerConfig.java
+++ src/main/java/bio/overture/ego/config/SwaggerConfig.java
@@ -16,9 +16,12 @@
 
 package bio.overture.ego.config;
 
+import static java.util.Arrays.asList;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.base.Optional;
 import com.google.common.base.Predicates;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,23 +29,28 @@ import java.util.Set;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ParameterBuilderPlugin;
+import springfox.documentation.spi.service.contexts.ParameterContext;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.common.SwaggerPluginSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -68,30 +76,76 @@ public class SwaggerConfig {
   }
 
   @Bean
+  public ParameterBuilderPlugin parameterBuilderPlugin() {
+    return new ParameterBuilderPlugin() {
+      @Override
+      public void apply(ParameterContext context) {
+        if (context.getGroupName().equals("token-endpoint")) {
+          context
+              .getOperationContext()
+              .operationBuilder()
+              .parameters(
+                  asList(
+                      new ParameterBuilder()
+                          .type(new TypeResolver().resolve(String.class))
+                          .name("client_secret")
+                          .parameterType("query")
+                          .required(true)
+                          .modelRef(new ModelRef("String"))
+                          .build(),
+                      new ParameterBuilder()
+                          .type(new TypeResolver().resolve(String.class))
+                          .name("client_id")
+                          .parameterType("query")
+                          .required(true)
+                          .modelRef(new ModelRef("String"))
+                          .build(),
+                      new ParameterBuilder()
+                          .type(new TypeResolver().resolve(String.class))
+                          .name("grant_type")
+                          .parameterType("query")
+                          .required(true)
+                          .modelRef(new ModelRef("String"))
+                          .build()));
+
+          // hide default "parameters" arg
+          Optional<String> defaultName = context.resolvedMethodParameter().defaultName();
+          if (defaultName.isPresent() && defaultName.get().equals("parameters")) {
+            context.parameterBuilder().required(false).hidden(true).build();
+          }
+        }
+      }
+
+      @Override
+      public boolean supports(DocumentationType delimiter) {
+        return SwaggerPluginSupport.pluginDoesApply(delimiter);
+      }
+    };
+  }
+
+  @Bean
   public Docket productApi(SwaggerProperties swaggerProperties) {
-    val docket =
-        new Docket(SWAGGER_2)
-            .select()
-            .apis(
-                Predicates.or(
-                    basePackage("bio.overture.ego.controller"),
-                    basePackage("org.springframework.security.oauth2.provider.endpoint")))
-            .paths(PathSelectors.regex(pathsToExcludeRegex))
-            .build()
-            .host(swaggerProperties.host)
-            .pathProvider(
-                new RelativePathProvider(null) {
-                  @Override
-                  public String getApplicationBasePath() {
-                    return swaggerProperties.getBaseUrl();
-                  }
-                })
-            .securitySchemes(List.of(apiKey()))
-            .securityContexts(List.of(securityContext()))
-            .apiInfo(metaInfo())
-            .produces(Set.of("application/json"))
-            .consumes(Set.of("application/json"));
-    return docket;
+    return new Docket(SWAGGER_2)
+        .select()
+        .apis(
+            Predicates.or(
+                basePackage("bio.overture.ego.controller"),
+                basePackage("org.springframework.security.oauth2.provider.endpoint")))
+        .paths(PathSelectors.regex(pathsToExcludeRegex))
+        .build()
+        .host(swaggerProperties.host)
+        .pathProvider(
+            new RelativePathProvider(null) {
+              @Override
+              public String getApplicationBasePath() {
+                return swaggerProperties.getBaseUrl();
+              }
+            })
+        .securitySchemes(List.of(apiKey()))
+        .securityContexts(List.of(securityContext()))
+        .apiInfo(metaInfo())
+        .produces(Set.of("application/json"))
+        .consumes(Set.of("application/json"));
   }
 
   private ApiInfo metaInfo() {
