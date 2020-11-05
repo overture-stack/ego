9af72b1fba675f09188c70421166cbd9f8da723e
jaserud <udjaser@gmail.com>
2020-11-05 09:59:21 -0500

    revert, put aud claim back in user tokens for safety

--- src/main/java/bio/overture/ego/model/entity/Policy.java
+++ src/main/java/bio/overture/ego/model/entity/Policy.java
@@ -24,7 +24,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
