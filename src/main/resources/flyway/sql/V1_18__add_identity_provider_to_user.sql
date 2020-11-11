1f9286faa81a4ac66e180bf3974f581f0c2de5f7
Ann Catton <ann.catton@oicr.on.ca>
2020-11-11 11:58:31 -0500

    remove default provider values

--- src/main/resources/flyway/sql/V1_18__add_identity_provider_to_user.sql
+++ src/main/resources/flyway/sql/V1_18__add_identity_provider_to_user.sql
@@ -4,9 +4,11 @@ ALTER TABLE egouser DROP CONSTRAINT egouser_email_key;
 ALTER TABLE egouser ALTER COLUMN name DROP NOT NULL;
 ALTER TABLE egouser DROP CONSTRAINT egouser_name_key;
 
-CREATE TYPE providername AS ENUM('GOOGLE', 'FACEBOOK', 'LINKEDIN', 'GITHUB', 'ORCID', 'EMPTY');
-ALTER TABLE egouser ADD COLUMN identityprovider providername NOT NULL DEFAULT 'EMPTY';
+CREATE TYPE providername AS ENUM('GOOGLE', 'FACEBOOK', 'LINKEDIN', 'GITHUB', 'ORCID');
+ALTER TABLE egouser ADD COLUMN identityprovider providername;
+ALTER TABLE egouser ALTER COLUMN identityprovider SET NOT NULL;
 
-ALTER TABLE egouser ADD COLUMN providerid VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE egouser ADD COLUMN providerid VARCHAR(255);
+ALTER TABLE egouser ALTER COLUMN providerid SET NOT NULL;
 
 ALTER TABLE egouser ADD UNIQUE(identityprovider, providerid);
