7a55cb76c6b26dd771f42cdea6b0fc71c9cd841e
Ann Catton <ann.catton@oicr.on.ca>
2020-11-20 13:52:42 -0500

    migration will update existing users with provider type and id

--- src/main/resources/flyway/sql/V1_18__add_identity_provider_to_user.sql
+++ src/main/resources/flyway/sql/V1_18__add_identity_provider_to_user.sql
@@ -6,9 +6,14 @@ ALTER TABLE egouser DROP CONSTRAINT egouser_name_key;
 
 CREATE TYPE providerType AS ENUM('GOOGLE', 'FACEBOOK', 'LINKEDIN', 'GITHUB', 'ORCID');
 ALTER TABLE egouser ADD COLUMN providertype providerType;
+ALTER TABLE egouser ALTER COLUMN providertype SET DEFAULT 'GOOGLE';
+-- default values are not added to existing rows, need to explicitly update where providertype is NULL
+UPDATE egouser SET providertype = DEFAULT WHERE providertype IS NULL;
+-- then set not null constraint
 ALTER TABLE egouser ALTER COLUMN providertype SET NOT NULL;
 
 ALTER TABLE egouser ADD COLUMN providerid VARCHAR(255);
+UPDATE egouser SET providerid = email WHERE providerid IS NULL;
 ALTER TABLE egouser ALTER COLUMN providerid SET NOT NULL;
 
 ALTER TABLE egouser ADD UNIQUE(providertype, providerid);
