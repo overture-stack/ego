d4d7ac9b59d1b7a73a8bd9dffe12e4baabf9bc20
Ann Catton <ann.catton@oicr.on.ca>
2020-12-09 12:11:24 -0500

    use app context event to track default provider

--- src/main/java/bio/overture/ego/config/DefaultProviderConfig.java
+++ src/main/java/bio/overture/ego/config/DefaultProviderConfig.java
@@ -5,22 +5,24 @@ import static java.util.Objects.isNull;
 import bio.overture.ego.service.DefaultProviderService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class DefaultProviderConfig implements InitializingBean {
+public class DefaultProviderConfig implements ApplicationListener<ContextRefreshedEvent> {
+
   @Autowired DefaultProviderService defaultProviderService;
 
   @Value("${spring.flyway.placeholders.default_provider}")
   String configuredProvider;
 
   @Override
-  public void afterPropertiesSet() throws Exception {
-    log.info("Checking default provider configuration");
+  public void onApplicationEvent(ContextRefreshedEvent event) {
+    log.info("ApplicationContext refreshed, checking default provider configuration.");
     val storedProvider = defaultProviderService.findById(configuredProvider);
     if (isNull(configuredProvider) || storedProvider.isEmpty()) {
       throw new IllegalStateException(
