2fd2d2390e252173003f43ff354022b48e760bd8
blabadi <basharlabadi@gmail.com>
2021-11-15 09:25:06 -0500

    wip: upgrade spring boot upgrade spring cloud version fix profiles fix vault update for new pageable interface

--- src/main/java/bio/overture/ego/utils/IgnoreCaseSortDecorator.java
+++ src/main/java/bio/overture/ego/utils/IgnoreCaseSortDecorator.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 import static org.springframework.util.StringUtils.isEmpty;
 
 import java.util.Map;
+import java.util.Optional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -29,9 +30,24 @@ public class IgnoreCaseSortDecorator implements Pageable {
     return Sort.by(orders);
   }
 
+  @Override
+  public Sort getSortOr(Sort sort) {
+    return delegate.getSortOr(sort);
+  }
+
   private String getSortField(String sort) {
     val mapValue = SORT_MAP.get(sort);
     return isEmpty(mapValue) ? sort : mapValue;
+  }
+
+  @Override
+  public boolean isPaged() {
+    return delegate.isPaged();
+  }
+
+  @Override
+  public boolean isUnpaged() {
+    return delegate.isUnpaged();
   }
 
   /** Delegated methods */
@@ -66,7 +82,17 @@ public class IgnoreCaseSortDecorator implements Pageable {
   }
 
   @Override
+  public Pageable withPage(int pageNumber) {
+    return delegate.withPage(pageNumber);
+  }
+
+  @Override
   public boolean hasPrevious() {
     return delegate.hasPrevious();
+  }
+
+  @Override
+  public Optional<Pageable> toOptional() {
+    return Optional.of(delegate);
   }
 }
